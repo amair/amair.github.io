1871652408d9b41b7b9eabc1972764d1441a83df
Alastair Mair <git@catslippers.com>
2018-07-22 18:00:22 +0100

    Progress, but max data not loading

--- JS/stockChart.js
+++ JS/stockChart.js
@@ -16,7 +16,7 @@ AmCharts.ready(function() {
 
             var chart = new AmCharts.AmStockChart();
 
-            chart.dataDateFormat = ("YYYY-MM-DD hh:mm:ss");
+            chart.dataDateFormat = ("YYYY-MM-DD JJ:NN:SS");
 
             chart.pathToImages = "images/";
 
@@ -26,6 +26,7 @@ AmCharts.ready(function() {
             dataSet1.categoryField = "created_at";
             dataSet1.title = "Minimum";
             dataSet1.compared = true;
+            dataSet1.color =  "#0000FF";
 
 
             var dataSet2 = new AmCharts.DataSet();
@@ -35,8 +36,14 @@ AmCharts.ready(function() {
             dataSet2.categoryField = "created_at";
             dataSet2.title = "Maximum";
             dataSet2.compared = true;
+            dataSet2.color = "#FF0000";
+
 
             chart.dataSets = [dataSet1, dataSet2];
+
+            var valueAxis = new AmCharts.ValueAxis();
+            valueAxis.id = "axis";
+            valueAxis.type = "date";
 
             var stockPanel = new AmCharts.StockPanel();
 
@@ -49,26 +56,19 @@ AmCharts.ready(function() {
             var graph = new AmCharts.StockGraph();
 
             graph.valueField = "min";
+            graph.compareField = "max";
             graph.type = "line";
-            graph.title = "Min"
+            graph.title = "Min-Max"
             graph.comparable = true;
-            // graph.valueAxis = 'axis1';
-
-            stockPanel.addStockGraph(graph);
-
-            // var graph2 = new AmCharts.StockGraph();
-            // graph2.title = "Max"
-            // graph2.type = "line";
-            // graph2.comparable = true;
-            // graph2.valueField = "max";
-            // graph2.compareField = "max";
-            // graph2.compareGraph = {
+            graph.valueAxis = 'axis';
+            // graph.lineThickness = ;
+            // graph.compareGraphLineThickness = ;
+            // graph.compareGraph = {
             //     "type": "smoothedLine",
             //     "bullet": "round"
             // };
-            // graph2.valueAxis = 'axis2';
 
-            // stockPanel.addStockGraph(graph2);
+            stockPanel.addStockGraph(graph);
 
             var categoryAxesSettings = new AmCharts.CategoryAxesSettings();
             categoryAxesSettings.dashLength = 5;
@@ -76,24 +76,15 @@ AmCharts.ready(function() {
             categoryAxesSettings.parseDates = true;
             chart.categoryAxesSettings = categoryAxesSettings;
 
-            var valueAxesSettings = new AmCharts.ValueAxesSettings();
-            valueAxesSettings .dashLength = 5;
-            chart.valueAxesSettings  = valueAxesSettings;
 
-            // var valueAxis1 = new AmCharts.ValueAxis();
-            // valueAxis1.id = "axis1";
+            var chartCursorSettings = new AmCharts.ChartCursorSettings();
+            chartCursorSettings.valueBalloonsEnabled = true;
+            chart.chartCursorSettings = chartCursorSettings;
 
-            // var valueAxis2 = new AmCharts.ValueAxis();
-            // valueAxis2.id = "axis2";
+            var dataSetSelector = new AmCharts.DataSetSelector();
+            dataSetSelector.position = "left";
 
-            // var chartScrollbarSettings = new AmCharts.ChartScrollbarSettings();
-            // chartScrollbarSettings.graph = graph;
-            // chartScrollbarSettings.graphType = "line";
-            // chart.chartScrollbarSettings = chartScrollbarSettings;
-
-            // var chartCursorSettings = new AmCharts.ChartCursorSettings();
-            // chartCursorSettings.valueBalloonsEnabled = true;
-            // chart.chartCursorSettings = chartCursorSettings;
+            chart.dataSetSelector = dataSetSelector;
 
             // var periodSelector = new AmCharts.PeriodSelector();
             // periodSelector.periods = [{period:"DD", count:1, label:"1 day"},
@@ -102,7 +93,7 @@ AmCharts.ready(function() {
             //                           {period:"YYYY", count:1, label:"1 year"},
             //                           {period:"YTD", label:"YTD"},
             //                           {period:"MAX", label:"MAX"}];
-            // chart.dataDateFormat = ("YYYY-MM-DD HH:NN:SS");
+            // periodSelector.dateFormat  = ("YYYY-MM-DD JJ:NN:SS");
             // chart.periodSelector = periodSelector;
 
             chart.write("chartdiv");
